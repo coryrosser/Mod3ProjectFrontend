29fae315cbdbef92a972584b2fd68e86d54d0729
coryrosser <coryrosser@gmail.com>
2020-05-25 21:53:23 -0400

    added navigation and redid login form. now works with backend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,121 @@
 const USERS_URL = "http://localhost:3000/users"
 
-function addLoginEvent() {
-    let loginBtn = document.getElementById("login-form")
-    loginBtn.addEventListener("submit", onLoginSubmit)
+let homeScreen = () => {
+    console.log("at home :)")
+    let mainParentDiv = document.getElementById("page-content");
+    mainParentDiv.innerHTML = "";
+    let welcomeMessage = document.createElement("h1");
+    welcomeMessage.innerText = "ToneDeaf Traders";
+    welcomeMessage.setAttribute("id", "welcome-message");
+    mainParentDiv.appendChild(welcomeMessage)
+        //nav-bar 
+    let navbarDiv = document.getElementById('nav-div')
+    navbarDiv.innerHTML = ""
+        //adds nav element. takes in parent node, nav name, nav id, callback for event
+    addNavElement(navbarDiv, "Home", "nav-item-home", homeScreen)
+    addNavElement(navbarDiv, "Listings", "nav-item-listings", getListings)
+    addNavElement(navbarDiv, "Login", "nav-item-login", getLoginForm)
+
 
 }
 
+function addNavElement(parent, elementName, elementId, callback) {
+    let newElement = document.createElement("a");
+    newElement.innerText = elementName;
+    newElement.href = `#${elementName}`;
+    newElement.setAttribute("id", elementId);
+    newElement.setAttribute("class", "item");
+    newElement.addEventListener("click", callback)
+    parent.appendChild(newElement);
+
+}
+
+function getListings() {
+    //using items url for now. will change when we have listings
+    let listingsUrl = "http://localhost:3000/items"
+    fetch(listingsUrl)
+        .then(res => res.json())
+        .then(itemData => console.log(itemData))
+}
+
+function getLoginForm() {
+    let mainParentDiv = document.getElementById("page-content");
+    mainParentDiv.innerHTML = "";
+    //form creation 
+    let loginForm = document.createElement("form")
+
+    let emailLabel = document.createElement("label")
+    emailLabel.htmlFor = "email"
+    emailLabel.innerText = "Email: "
+
+    let emailInput = document.createElement("input")
+    emailInput.name = "email"
+    emailInput.type = "email"
+    emailInput.id = "login-email"
+
+    let passwordLabel = document.createElement("label")
+    passwordLabel.htmlFor = "password"
+    passwordLabel.innerText = "Password: "
+
+    let passwordInput = document.createElement("input")
+    passwordInput.name = "password"
+    passwordInput.type = "password"
+    passwordInput.id = "login-password"
+
+    let submitBtn = document.createElement('button')
+    submitBtn.innerHTML = "Login"
+    submitBtn.type = "submit"
+
+    loginForm.appendChild(emailLabel)
+    loginForm.appendChild(emailInput)
+    loginForm.appendChild(passwordLabel)
+    loginForm.appendChild(passwordInput)
+    loginForm.appendChild(submitBtn)
+
+    addLoginEvent(loginForm)
+    mainParentDiv.appendChild(loginForm)
+}
+
+function addLoginEvent(target) {
+    target.addEventListener("submit", onLoginSubmit)
+
+}
+//login submit sends a POST to sessions controller with email and pw from login form
+//session controller verifies a user with that email exists and pw is correct
+//if correct, sends back the user object and sets current_user to the corresponding
+//user
+function onLoginSubmit(event) {
+    let email = document.getElementById("login-email").value
+    let pw = document.getElementById("login-password").value
+    fetch("http://localhost:3000/login", {
+            method: 'POST',
+            mode: 'cors',
+            credentials: 'same-origin',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                email,
+                pw
+            })
+        })
+        .then(res => res.json())
+        .then(res => {
+            if (res.code == 200) {
+                showUserProfile(res.user)
+                console.log(res.message)
+            }
+
+        })
+    event.preventDefault()
+}
+
+function showUserProfile(user) {
+    console.log(user)
+}
+
+//fetch and render users
 function fetchUsers(url) {
     fetch(url)
         .then(res => res.json())
@@ -24,6 +134,7 @@ function renderUsers(userData) {
         createUserli(user, ul)
     })
 }
+//==========
 
 function createUserli(user, parentNode) {
     let userLi = document.createElement("li")
@@ -31,27 +142,5 @@ function createUserli(user, parentNode) {
     parentNode.appendChild(userLi)
 }
 
-function onLoginSubmit(event) {
-    let email = document.getElementById("login-email").value
-    let pw = document.getElementById("login-password").value
-    fetch("http://localhost:3000/login", {
-            method: 'POST',
-            mode: 'cors',
-            credentials: 'same-origin',
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify({
-                email,
-                pw
-            })
-        })
-        .then(res => res.json())
-        .then(user => console.log(user))
-    event.preventDefault()
-}
 
-addLoginEvent();
-
-fetchUsers(USERS_URL);
+homeScreen();
